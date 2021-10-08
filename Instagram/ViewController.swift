c87d294c5d122b9c1f1be3e65f4448b9e346b42c
rijadkasumi <rijadd8@gmail.com>
2021-10-08 10:09:50 -0400

    Initial Commit

---
+++ Instagram/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Instagram
+//
+//  Created by Rijad Kasumi on 10/8/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
